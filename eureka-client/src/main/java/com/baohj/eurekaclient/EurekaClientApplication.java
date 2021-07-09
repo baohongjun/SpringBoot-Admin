eabc42c56b01e6528c56e8043e1e73e3ef337e18
baohongjun <18375382691@163.com>
2021-07-09 09:48:06 +0800

    delete service-client-feign

---
+++ eureka-client/src/main/java/com/baohj/eurekaclient/EurekaClientApplication.java
@@ -0,0 +1,20 @@
+package com.baohj.eurekaclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * @author DELL
+ */
+@SpringBootApplication
+@EnableEurekaClient         //表明自己是一个eurekaclient.
+public class EurekaClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaClientApplication.class, args);
+    }
+
+
+
+}
